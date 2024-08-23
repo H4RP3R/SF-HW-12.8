ead83a42865318edd597adb3717ded2afe7cf75a
zombiehunter <viktormallory25@gmail.com>
2024-08-23 14:20:49 +0300

    add sorting functions with tests

--- sort.go
+++ sort.go
@@ -1,5 +1,6 @@
 package sort
 
+// Bubble sort
 func bubbleSort(ar []int) {
 	sorted := false
 
@@ -12,4 +13,87 @@ func bubbleSort(ar []int) {
 			}
 		}
 	}
+}
+
+// Selection sort
+func selectionSort(ar []int) {
+	if len(ar) < 2 {
+		return
+	}
+
+	for i := 0; i < len(ar); i++ {
+		minIdx := i
+		for j := i + 1; j < len(ar); j++ {
+			if ar[j] < ar[minIdx] {
+				minIdx = j
+			}
+		}
+		ar[i], ar[minIdx] = ar[minIdx], ar[i]
+	}
+}
+
+// Insertion sort
+func insertionSort(ar []int) {
+	for i := 1; i < len(ar); i++ {
+		for j := i; j > 0; j-- {
+			if ar[j] < ar[j-1] {
+				ar[j], ar[j-1] = ar[j-1], ar[j]
+			} else {
+				break
+			}
+		}
+	}
+}
+
+// Merge sort
+func mergeSort(ar []int) []int {
+	if len(ar) < 2 {
+		return ar
+	}
+
+	mid := len(ar) / 2
+	left := mergeSort(ar[:mid])
+	right := mergeSort(ar[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	merged := []int{}
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			merged = append(merged, left[i])
+			i++
+		} else {
+			merged = append(merged, right[j])
+			j++
+		}
+	}
+
+	merged = append(merged, left[i:]...)
+	merged = append(merged, right[j:]...)
+
+	return merged
+}
+
+// Quick sort
+func quickSort(ar []int) []int {
+	if len(ar) < 2 {
+		return ar
+	}
+
+	less := []int{}
+	greater := []int{}
+	pivot := ar[0]
+	for _, n := range ar[1:] {
+		if n < pivot {
+			less = append(less, n)
+		} else {
+			greater = append(greater, n)
+		}
+	}
+
+	return append(append(quickSort(less), pivot), quickSort(greater)...)
 }
--- sort_test.go
+++ sort_test.go
@@ -42,3 +42,79 @@ func Test_bubbleSort(t *testing.T) {
 		})
 	}
 }
+
+func Test_selectionSort(t *testing.T) {
+	for i := 0; i < testsCount; i++ {
+		size := rand.Intn(testArrMaxSize)
+		min := testArrMinVal
+		max := rand.Intn(testArrMaxVal)
+		testName := fmt.Sprintf("arr size: %d, min: %d, max: %d", size, min, max)
+		t.Run(testName, func(t *testing.T) {
+			testArr := randomIntArr(size, min, max)
+			gotArr := make([]int, size)
+			copy(gotArr[:], testArr[:])
+			sort.Ints(testArr)
+			selectionSort(gotArr)
+			if !reflect.DeepEqual(testArr, gotArr) {
+				t.Errorf("slices aren't equal")
+			}
+		})
+	}
+}
+
+func Test_insertionSort(t *testing.T) {
+	for i := 0; i < testsCount; i++ {
+		size := rand.Intn(testArrMaxSize)
+		min := testArrMinVal
+		max := rand.Intn(testArrMaxVal)
+		testName := fmt.Sprintf("arr size: %d, min: %d, max: %d", size, min, max)
+		t.Run(testName, func(t *testing.T) {
+			testArr := randomIntArr(size, min, max)
+			gotArr := make([]int, size)
+			copy(gotArr[:], testArr[:])
+			sort.Ints(testArr)
+			insertionSort(gotArr)
+			if !reflect.DeepEqual(testArr, gotArr) {
+				t.Errorf("slices aren't equal")
+			}
+		})
+	}
+}
+
+func Test_mergeSort(t *testing.T) {
+	for i := 0; i < testsCount; i++ {
+		size := rand.Intn(testArrMaxSize)
+		min := testArrMinVal
+		max := rand.Intn(testArrMaxVal)
+		testName := fmt.Sprintf("arr size: %d, min: %d, max: %d", size, min, max)
+		t.Run(testName, func(t *testing.T) {
+			testArr := randomIntArr(size, min, max)
+			gotArr := make([]int, size)
+			copy(gotArr[:], testArr[:])
+			sort.Ints(testArr)
+			gotArr = mergeSort(gotArr)
+			if !reflect.DeepEqual(testArr, gotArr) {
+				t.Errorf("slices aren't equal")
+			}
+		})
+	}
+}
+
+func Test_quickSort(t *testing.T) {
+	for i := 0; i < testsCount; i++ {
+		size := rand.Intn(testArrMaxSize)
+		min := testArrMinVal
+		max := rand.Intn(testArrMaxVal)
+		testName := fmt.Sprintf("arr size: %d, min: %d, max: %d", size, min, max)
+		t.Run(testName, func(t *testing.T) {
+			testArr := randomIntArr(size, min, max)
+			gotArr := make([]int, size)
+			copy(gotArr[:], testArr[:])
+			sort.Ints(testArr)
+			gotArr = quickSort(gotArr)
+			if !reflect.DeepEqual(testArr, gotArr) {
+				t.Errorf("slices aren't equal")
+			}
+		})
+	}
+}
