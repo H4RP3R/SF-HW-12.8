bb0c780bc56b7ad1d991920246f8301f6c06f4ab
zombiehunter <viktormallory25@gmail.com>
2024-08-23 15:05:02 +0300

    add benchmarks

--- sort_bench_test.go
+++ sort_bench_test.go
@@ -45,3 +45,131 @@ func BenchmarkBubbleSort(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkSelectionSort(b *testing.B) {
+	b.Run("small arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			selectionSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("middle arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(100, 1000)
+			b.StartTimer()
+			selectionSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("big arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10000, 100000)
+			b.StartTimer()
+			selectionSort(ar)
+			b.StopTimer()
+		}
+	})
+}
+
+func BenchmarkInsertionSort(b *testing.B) {
+	b.Run("small arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			insertionSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("middle arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(100, 1000)
+			b.StartTimer()
+			insertionSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("big arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10000, 100000)
+			b.StartTimer()
+			insertionSort(ar)
+			b.StopTimer()
+		}
+	})
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	b.Run("small arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			mergeSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("middle arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(100, 1000)
+			b.StartTimer()
+			mergeSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("big arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10000, 100000)
+			b.StartTimer()
+			mergeSort(ar)
+			b.StopTimer()
+		}
+	})
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	b.Run("small arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10, 10)
+			b.StartTimer()
+			quickSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("middle arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(100, 1000)
+			b.StartTimer()
+			quickSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("big arrays", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(10000, 100000)
+			b.StartTimer()
+			quickSort(ar)
+			b.StopTimer()
+		}
+	})
+}
